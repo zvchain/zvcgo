34a7730c19de598363a3a738eb95ba392ca7c577
ysh0566 <ysh0566@qq.com>
2019-08-14 20:25:48 +0800

    keybags fix

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package zvlib
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"math"
@@ -60,4 +61,31 @@ func parseDerivationPath(path string) ([]uint32, error) {
 		result = append(result, value)
 	}
 	return result, nil
+}
+
+func has0xPrefix(input string) bool {
+	return len(input) >= 2 && input[0] == '0' && (input[1] == 'x' || input[1] == 'X')
+}
+
+// Decode decodes a hex string with 0x prefix.
+func Decode(input string) ([]byte, error) {
+	if len(input) == 0 {
+		return nil, ErrorInvalid0xString
+	}
+	if !has0xPrefix(input) {
+		return nil, ErrorInvalid0xString
+	}
+	b, err := hex.DecodeString(input[2:])
+	if err != nil {
+		err = ErrorInvalid0xString
+	}
+	return b, err
+}
+
+// Encode encodes b as a hex string with 0x prefix.
+func Encode(b []byte) string {
+	enc := make([]byte, len(b)*2+2)
+	copy(enc, "0x")
+	hex.Encode(enc[2:], b)
+	return string(enc)
 }
